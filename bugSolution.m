eff49648addf62e9c152bc405c640332f2b97b57
BugHunter-X <askzenai@gmail.com>
2024-12-26 11:46:04 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,8 @@
+function result = myFunction(input)
+  % Some code here...
+  if input < 0
+    result = 0; % Or throw an error: error('Input must be non-negative'); 
+    return
+  end
+  % More code here...
+end
